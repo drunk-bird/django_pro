3e0ea1a894fea799c37200d95b37ac248e90c80c
drunk-bird <drarnol94@gmail.com>
2017-09-04 12:26:46 +0800

    db

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -75,12 +75,27 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'arnol',
+        'PASSWORD': 'arnol',
+        'HOST': '172.30.88.5',
+        'PORT': '3306',
+
     }
+
 }
+
 
 # DATABASES = {
 #     'default':{
