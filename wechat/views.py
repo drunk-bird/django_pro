ed3c85045fca89db9ca75d34c1273f4373518d63
drunk-bird <drarnol94@gmail.com>
2017-09-04 13:58:19 +0800

    create db

--- wechat/views.py
+++ wechat/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from wechat import models
 
 # Create your views here.
 
-from django_pro.settings import DATABASES
-import os
 
 
 def index(request):
-    try:
-        DATABASES = {
-                'ENGINE': 'django.db.backends.mysql',
-                'NAME': os.getenv('MYSQL_DATABASE'),
-                'USER': os.getenv('MYSQL_USER'),
-                'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-                'HOST': os.getenv('MYSQL_SERVICE_HOST'),
-                'PORT': os.getenv('MYSQL_SERVICE_PORT'),
-
-            }
-    except Exception as error:
-        print(error)
-        DATABASES = {'ENGINE': 'django.db.backends.mysql',}
+    models.UserInfo.objects.create(name='arnol',password='123')
+    DATABASES = models.UserInfo.objects.first()
     return render(request,'wechat/index.html',{'databases':DATABASES})
