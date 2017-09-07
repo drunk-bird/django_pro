acb9dcab63965991ab2662f27699e68326824ff9
drunk-bird <drarnol94@gmail.com>
2017-09-07 18:19:19 +0800

    cof

--- wechat/config.py
+++ wechat/config.py
@@ -5,10 +5,3 @@ WECHATCONF = {  'token':'weixin',
                   }
 
 WELCOME_TEXT = "感谢关注[愉快]\n我是星喵大人[调皮]\n\n"
-
-from yaml import load, dump
-
-
-w = open('config.ymal',encoding='utf-8')
-print(load(w)['welcome_test'])
-
