1f9e4199cf79da2b340652d1f1f36d9113f2e8e1
drunk-bird <drarnol94@gmail.com>
2017-09-07 23:37:16 +0800

    j

--- wechat/modules/config_handle.py
+++ wechat/modules/config_handle.py
@@ -3,7 +3,7 @@ from yaml import load,dump
 
 
 def load_conf(conf_key = None):
-    f = open('wechat/config.ymal',encoding='utf-8')
+    f = open('wechat/config1.ymal',encoding='utf-8')
     conf = load(f)
     f.close()
     return conf[conf_key] if conf_key else conf
